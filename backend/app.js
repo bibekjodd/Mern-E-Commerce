55de574ac521be2de8d52b8631613cb4fc8c0594
Bibek Bhattarai <bibekjodd@outlook.com>
2022-12-06 23:07:15 +0545

    order-api-done

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,11 @@ const errorMiddleware = require('./middleware/error');
 // route imports
 const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
-
+const order = require('./routes/orderRoute');
 
 app.use('/api/v1', product);
 app.use('/api/v1', user)
+app.use('/api/v1', order)
 app.use(errorMiddleware);
 
 // middleware for error
